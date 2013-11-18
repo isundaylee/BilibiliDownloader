2745a3bcd0f70dc833eb8bebdde4c9356ced83f3
Sunday Lee <isundaylee.reg@gmail.com>
2013-11-17 23:36:32 -0500

    Adds utility to concatenate parts.

---
+++ utils/concat.rb
@@ -0,0 +1,20 @@
+require 'fileutils'
+
+ffmpeg = "/Users/Sunday/Downloads/Safari\ Downloads/ffmpeg"
+
+Dir['*'].each do |d|
+  next if File.exists?("#{d}.flv")
+  next unless File.directory?(d)
+
+  FileUtils.mkdir_p('/tmp/cct'); 
+
+  File.open(File.join('/tmp/cct', d), 'w') do |l|
+    Dir["#{d}/*"].each do |f|
+      l.puts("file '#{File.expand_path(f)}'")
+    end
+  end
+
+  command = "\"#{ffmpeg}\" -f concat -i \"#{File.join('/tmp/cct', d)}\" -c copy \"#{d}.flv\""
+
+  puts command
+end
